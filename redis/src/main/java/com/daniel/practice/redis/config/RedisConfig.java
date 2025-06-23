5a01b1b1b47e222120c29f6cf3aed7c220a8ca72
Daniel <byoung.mokk@gmail.com>
2025-06-23 16:09:06 +0900

    ✨Feat: Spring Boot + Redis 연동
    1. Redis 기본 연결 및 설정
    - application.yml
    - build.gradle
    
    2. Spring Data Redis 연동
    - RedisConfig.java

---
+++ redis/src/main/java/com/daniel/practice/redis/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.daniel.practice.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		StringRedisSerializer stringSerializer = new StringRedisSerializer();
+		GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
+
+		template.setKeySerializer(stringSerializer);
+		template.setValueSerializer(jsonSerializer);
+
+		// Hash
+		template.setHashKeySerializer(stringSerializer);
+		template.setHashValueSerializer(jsonSerializer);
+
+		template.afterPropertiesSet(); // 모든 설정이 끝난 후 초기화
+		return template;
+	}
+}
